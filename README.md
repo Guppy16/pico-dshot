83c4a6e06974581c9f27c3e0d3e5393931be791b
Akash Gupta <ag2118@cam.ac.uk>
2023-02-11 13:46:42 +0000

    restructue main for single frame dma buffer. timer not working atm

--- README.md
+++ README.md
@@ -31,6 +31,37 @@ NOTE: need a good source for this.
 48 - 2047: Throttle (2000 steps of precision)
 
 ---
+## Scheme
+
+The ESC needs commands to be sent within a set interval (currently undetermined). In order to satisfy this, the code is modelled as follows:
+
+NOTE: Timer interrupt should be longer than the time taken to send a command from the buffer using DMA. This is so that there is enough time for the Main loop to send the next command before the interrupt is raised!
+
+Timer raises interrupt at set-interval rate
+    This will restart DMA (i.e. re-send last known command)
+    OR a safety measure can be done to check if the system is responding, else send a disarm cmd / NOT send a cmd so that ESC disarms by itself
+
+Main loop:
+    Calculate next command to send
+    Reset Timer interrupt
+    Wait for current command to finish
+    Send command
+
+Debugging:
+    Measure how many times interrupt is called
+    Measure how long each main loop takes
+    Measure minimum time before motor sends command
+
+Additions:
+    Look at using a two buffers. One to store the current command being executed. The second one should store the next command to write. The idea is that this elimantes the  overhead in copying from the temporary buffer to the main buffer; instead, we are swapping the reference to the buffer where DMA gets data from. however, this complicated things due to having to keep track of which buffer is which..
+
+NOTE: 32 bit time is used
+- `timer_hw->timerawl` returns a 32 bit number representing the number of microseconds since power on
+- This will overflow at around 72 hrs
+- We assume that this is longer than the operation time before the microcontroller is restarted
+- NOTE that this has been the cause of many accidental failures in the past..
+
+---
 ## Functions
 
 - :tick: calculating duty cycle of bits from dshot speed
@@ -40,6 +71,9 @@ NOTE: need a good source for this.
 
 - Write some tests for these function!
 
+NOTE:
+- When re-setting DMA configuration, I tried just resetting the read address (assuming that the remaining configuration would stay the same). This did not work; one must re-configure the channel on every transfer. This seems inefficient.
+
 ---
 ## Test Data
 
@@ -47,3 +81,4 @@ Command: 1, Tel: 1
 0x0033
 Transmitted from left to right (I think)
 LLLL LLLL LLHH LLHH
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <Arduino.h>
 #include "hardware/pwm.h"
 #include "hardware/dma.h"
+#include "hardware/timer.h"
+#include "hardware/irq.h"
 #include "dshot.h"
 #include "utils.h"
 
-#define DSHOT_SPEED 600 // kHz
+#define DSHOT_SPEED 150 // kHz
 #define MCU_FREQ 120    // MHz
 
 #define DEBUG 0
+
+constexpr uint MOTOR_GPIO = DEBUG ? 25 : 14;
+
+// --- DMA Variables
+// #define PICO_TIME_DEFAULT_ALARM_POOL_DISABLED 1
+// Setup Alarms using HW Timers to be used for DMA
+// NOTE: Alarm number and Timer IRQ number have to be the same
+#define DMA_ALARM_NUM 1
+#define DMA_ALARM_IRQ TIMER_IRQ_1
 
 // Note that these should be cast uint32_t when sent to the slice
 // WRAP = Total number of counts in PWM cycle
@@ -15,249 +26,343 @@ constexpr uint16_t DMA_WRAP = DEBUG ? (1 << 16) - 1 : 1000 * MCU_FREQ / DSHOT_SP
 constexpr uint16_t DSHOT_LOW = 0.37 * DMA_WRAP;
 constexpr uint16_t DSHOT_HIGH = 0.75 * DMA_WRAP;
 
-constexpr uint32_t cmd_nums = 1500;
-constexpr uint32_t cmd_size = 20;
-constexpr size_t dma_buffer_length = cmd_size * (cmd_nums + 2);
+constexpr uint32_t DSHOT_CMD_SIZE = 16;
+constexpr size_t dma_buffer_length = DSHOT_CMD_SIZE + 4;
+// TODO: Does this ever need to be volatile?
 uint32_t dma_buffer[dma_buffer_length] = {0};
 
-void code_to_dma_buffer(const uint16_t &code, const bool &telemtry, const uint &channel)
-{
-  size_t dma_buffer_idx = 0;
-  // Increase throttle
-  for (size_t i = 0; i < 750; ++i)
-  {
-    DShot::command_to_pwm_buffer(i, 0, dma_buffer + dma_buffer_idx, DSHOT_LOW, DSHOT_HIGH, channel);
-    dma_buffer_idx += cmd_size;
-  }
-
-  // Decrease throttle
-  for (size_t i = 750; i >= 48; --i)
-  {
-    DShot::command_to_pwm_buffer(i, 0, dma_buffer + dma_buffer_idx, DSHOT_LOW, DSHOT_HIGH, channel);
-    dma_buffer_idx += cmd_size;
-  }
-
-  // Keep throttle at 0
-  while (dma_buffer_idx < dma_buffer_length)
-  {
-    DShot::command_to_pwm_buffer(48, 0, dma_buffer + dma_buffer_idx, DSHOT_LOW, DSHOT_HIGH, channel);
-    dma_buffer_idx += cmd_size;
-  }
-
-  // DShot::command_to_pwm_buffer(750, 0, dma_buffer + dma_buffer_idx, DSHOT_LOW, DSHOT_HIGH, channel);
-}
-
+// DMA config
 int dma_chan = dma_claim_unused_channel(true);
 dma_channel_config dma_conf = dma_channel_get_default_config(dma_chan);
 
-constexpr uint MOTOR_GPIO = DEBUG ? 25 : 14;
+// PWM config
 uint pwm_slice_num = pwm_gpio_to_slice_num(MOTOR_GPIO);
 uint pwm_channel = pwm_gpio_to_channel(MOTOR_GPIO);
 
+// Somehow Assert that DShot_SPEED / DSHOT_CMD_SIZE > timer_rate
+
+// Timer interrupt at 8 kHz
+// Every interrupt will send the previous command
+// And reset the interrupt
+// NOTE: this freq is too high is using DSHOT 150!!
+constexpr uint32_t DMA_ALARM_PERIOD = 1000 / 8; // 8 kHz --> micro secs
+
+// ISR to send frame over DMA
+// TODO: Check if irq_set_enabled is needed? Maybe it only needs to be configured once?
+/*
+void alarm_irq_send_dma_frame(void)
+{
+    // Clear the alarm irq
+    hw_clear_bits(&timer_hw->intr, 1u << DMA_ALARM_NUM);
+
+    // Change state of LED??
+
+    // Re-set DMA read address AND set trigger
+    // to start sending the frame again
+    dma_channel_set_read_addr(dma_chan, dma_buffer, true);
+
+    // -- Reset the alarm
+    // Enable the alarm irq
+    irq_set_enabled(DMA_ALARM_IRQ, true);
+
+    // NOTE: Alarm is only 32 bits
+    // so, be careful if delay is more than that
+    uint64_t target = timer_hw->timerawl + DMA_ALARM_PERIOD;
+    timer_hw->alarm[DMA_ALARM_NUM] = (uint32_t)target;
+}
+*/
+
 void setup()
 {
-  Serial.begin(9600);
-  // Flash LED
-  utils::flash_led(LED_BUILTIN);
+    Serial.begin(9600);
 
-  // --- Setup PWM
-  gpio_set_function(MOTOR_GPIO, GPIO_FUNC_PWM);
+    // Flash LED
+    utils::flash_led(LED_BUILTIN);
 
-  // Without config method
-  // NOTE: pwm should start after DMA initialised!
-  // OR can initialise PWM with 0 channel level
-  pwm_set_wrap(pwm_slice_num, DMA_WRAP);
-  pwm_set_chan_level(pwm_slice_num, pwm_channel, 0);
-  pwm_set_clkdiv(pwm_slice_num, DEBUG ? 240.0f : 1.0f); // Should run at 500 kHz for cpu-clck = 120 Mhz
-  pwm_set_enabled(pwm_slice_num, true);
+    // --- Setup PWM config
+    // Initialise PWM to ouput 0 signal
+    // TODO: start PWM after DMA config
+    gpio_set_function(MOTOR_GPIO, GPIO_FUNC_PWM);
+    pwm_set_wrap(pwm_slice_num, DMA_WRAP);
+    pwm_set_chan_level(pwm_slice_num, pwm_channel, 0);    // Set PWM to 0 output
+    pwm_set_clkdiv(pwm_slice_num, DEBUG ? 240.0f : 1.0f); // Should run at 500 kHz for cpu-clck = 120 Mhz
+    pwm_set_enabled(pwm_slice_num, true);
 
-  // Use config method
-  // pwm_config conf = pwm_get_default_config();
-  // pwm_config_set_wrap(&conf, 3);
-  // pwm_set_chan_level(pwm_slice_num, pwm_channel, 2);
-  // pwm_init(pwm_slice_num, &conf, true);
-  // pwm_set_chan_level(pwm_slice_num, pwm_channel, 2);
+    // --- Setup DMA
+    // PWM counter compare is 32 bit (16 bit per channel)
+    channel_config_set_transfer_data_size(&dma_conf, DMA_SIZE_32);
+    // Increment read address
+    channel_config_set_read_increment(&dma_conf, true);
+    // NO increment write address
+    channel_config_set_write_increment(&dma_conf, false);
+    // DMA Data request when PWM is finished
+    channel_config_set_dreq(&dma_conf, DREQ_PWM_WRAP0 + pwm_slice_num);
 
-  // --- Setup DMA packet
-  uint16_t code = 0;
-  bool telemetry = 0;
-  code_to_dma_buffer(code, telemetry, pwm_channel);
+    // --- Setup Alarm to send DMA frame
+    // Enable interrupt for alarm
+    // hw_set_bits(&timer_hw->inte, 1u << DMA_ALARM_NUM);
+    // Set irq handler for alarm irq
+    // irq_set_exclusive_handler(DMA_ALARM_IRQ, alarm_irq_send_dma_frame);
+    // Enable the alarm irq
+    // irq_set_enabled(DMA_ALARM_IRQ, true);
 
-  // --- Setup DMA
-  // PWM counter compare is 32 bit (16 bit per channel)
-  channel_config_set_transfer_data_size(&dma_conf, DMA_SIZE_32);
-  // Increment read address
-  channel_config_set_read_increment(&dma_conf, true);
-  // NO increment write address
-  channel_config_set_write_increment(&dma_conf, false);
-  // DMA Data request when PWM is finished
-  channel_config_set_dreq(&dma_conf, DREQ_PWM_WRAP0 + pwm_slice_num);
+    delay(1500);
 
-  dma_channel_configure(
-      dma_chan,
-      &dma_conf,
-      &pwm_hw->slice[pwm_slice_num].cc, // Write to PWM counter compare
-      dma_buffer,
-      dma_buffer_length,
-      true);
+    // Print General Settings
+    Serial.print("LED_BUILTIN GPIO: ");
+    Serial.println(LED_BUILTIN);
+    Serial.print("MOTOR GPIO: ");
+    Serial.println(MOTOR_GPIO);
+    Serial.print("PWM Slice num: ");
+    Serial.println(pwm_slice_num);
+    Serial.print("PWM Channel: ");
+    Serial.println(pwm_channel);
+    Serial.print("DMA channel: ");
+    Serial.println(dma_chan);
+    Serial.print("DMA Buffer Length: ");
+    Serial.println(dma_buffer_length);
+    Serial.print("DMA Alarm Period (us): ");
+    Serial.println(DMA_ALARM_PERIOD);
 
-  delay(11000);
-  Serial.print("LED_BUILTIN GPIO: ");
-  Serial.println(LED_BUILTIN);
-  Serial.print("MOTOR GPIO: ");
-  Serial.println(MOTOR_GPIO);
-  Serial.print("PWM Slice num: ");
-  Serial.println(pwm_slice_num);
-  Serial.print("PWM Channel: ");
-  Serial.println(pwm_channel);
-  Serial.print("DMA channel: ");
-  Serial.println(dma_chan);
+    // Print DShot settings
+    Serial.print("DShot: Wrap: ");
+    Serial.print(DMA_WRAP);
+    Serial.print(" Low: ");
+    Serial.print(DSHOT_LOW);
+    Serial.print(" High: ");
+    Serial.println(DSHOT_HIGH);
 
-  // Print DShot settings
-  Serial.print("DShot: Wrap: ");
-  Serial.print(DMA_WRAP);
-  Serial.print(" Low: ");
-  Serial.print(DSHOT_LOW);
-  Serial.print(" High: ");
-  Serial.println(DSHOT_HIGH);
-
-  // Print packet setup
-  Serial.print("Packet Setup:");
-  Serial.print(" Code: ");
-  Serial.print(code, HEX);
-  Serial.print(" Telemetry: ");
-  Serial.print(telemetry);
-  Serial.print(" Packet: ");
-  Serial.print(DShot::command_to_packet(code << 1 | telemetry), HEX);
-  Serial.println();
-
-  // utils::print_conf(conf);
+    // Start the dma transfer interrupt
+    // alarm_irq_send_dma_frame();
 }
+
 int incomingByte;
-void loop()
+uint32_t dma_buffer_length_temp[dma_buffer_length] = {0};
+uint32_t temp_dma_buffer[dma_buffer_length] = {0};
+
+/// NOTE
+// I think arm_sequence should be coded such that it returns
+// a throttle code. This will make the main loop more efficient
+// Note that the state of the arm sequence will still need
+// to be tracked somehow.
+// This could be done efficiently using the first 16 bits!?s
+
+/*
+    Function to perform arm sequence
+    This tracks the sequence of commands using a state machine?
+    Incr throttle from 0 to 750
+    Decr throttle from 750 to 48 (incl)
+    // TODO: Check if command can start from 48
+    // TODO: Check if command length can be shortened by skipping
+*/
+constexpr uint16_t ARM_THROTTLE = 300; // Max is 2000 (or 1999?)
+constexpr uint16_t THROTTLE_ZERO = 48; // 0 Throttle code
+uint16_t arm_sequence(const uint16_t idx)
 {
 
-  if (Serial.available() > 0)
-  {
-    incomingByte = Serial.read();
-
-    // l (led)
-    if (incomingByte == 108)
+    // Send 100 values spaced between 0 to ARM_THROTTLE
+    uint n = 50;
+    if (idx <= n)
     {
-      utils::flash_led(LED_BUILTIN);
+        return 48 + idx * (ARM_THROTTLE - 48) / n;
     }
 
-    // t (test)
-    if (incomingByte == 116)
+    // Increase throttle till 25%
+    // if (idx <= ARM_THROTTLE)
+    // {
+    //     return idx;
+    // }
+    // Decrease throttle to 0
+    // else if (idx <= 2 * ARM_THROTTLE - 48)
+    // {
+    //     return 2 * ARM_THROTTLE - idx;
+    // }
+    // // Return 0 for a couple more times
+    // else if (idx <= 2 * ARM_THROTTLE - 46)
+    // {
+    //     return 48;
+    // }
+    // Increase throttle back up
+    // else if (idx <= 3 * ARM_THROTTLE)
+    // {
+    //     return idx - 2 * ARM_THROTTLE;
+    // }
+    return 0;
+}
+
+uint16_t throttle_code = 0;
+uint16_t telemtry = 0;
+
+// Helper function to send code to DMA buffer
+uint16_t writes_to_temp_dma_buffer = 0;
+uint16_t writes_to_dma_buffer = 0;
+void send_dshot_frame()
+{
+    // Stop timer interrupt JIC
+    // irq_set_enabled(DMA_ALARM_IRQ, false);
+
+    // IF DMA is busy, then write to temp_dma_buffer
+    // AND wait for DMA buffer to finish transfer
+    // Then copy the temp buffer to dma buffer
+    Serial.print(throttle_code);
+    Serial.print("\t");
+    if (dma_channel_is_busy(dma_chan))
     {
-      code_to_dma_buffer(0, 0, pwm_channel);
-      delay(2000);
-      dma_channel_configure(
-          dma_chan,
-          &dma_conf,
-          &pwm_hw->slice[pwm_slice_num].cc, // Write to PWM counter compare
-          dma_buffer,
-          dma_buffer_length,
-          true);
-      delay(500);
-      dma_channel_configure(
-          dma_chan,
-          &dma_conf,
-          &pwm_hw->slice[pwm_slice_num].cc, // Write to PWM counter compare
-          dma_buffer,
-          dma_buffer_length,
-          true);
+        DShot::command_to_pwm_buffer(throttle_code, telemtry, temp_dma_buffer, DSHOT_LOW, DSHOT_HIGH, pwm_channel);
+        dma_channel_wait_for_finish_blocking(dma_chan);
+        memcpy(dma_buffer, temp_dma_buffer, dma_buffer_length * sizeof(uint32_t));
+        ++writes_to_temp_dma_buffer;
+    }
+    // ELSE write to dma_buffer directly
+    else
+    {
+        DShot::command_to_pwm_buffer(throttle_code, telemtry, dma_buffer, DSHOT_LOW, DSHOT_HIGH, pwm_channel);
+        ++writes_to_dma_buffer;
+    }
+    // Re-configure DMA and trigger transfer
+    dma_channel_configure(
+        dma_chan,
+        &dma_conf,
+        &pwm_hw->slice[pwm_slice_num].cc, // Write to PWM counter compare
+        dma_buffer,
+        dma_buffer_length,
+        true);
+
+    // Restart interrupt
+    // irq_set_enabled(DMA_ALARM_IRQ, true);
+    // NOTE: Alarm is only 32 bits
+    // so, be careful if delay is more than that
+    // uint64_t target = timer_hw->timerawl + DMA_ALARM_PERIOD;
+    // timer_hw->alarm[DMA_ALARM_NUM] = (uint32_t)target;
+}
+
+// Helper function to arm motor
+void arm_motor()
+{
+    uint16_t arm_idx = 0 - 1;
+    telemtry = 0;
+
+    // Debugging
+    writes_to_temp_dma_buffer = 0;
+    writes_to_dma_buffer = 0;
+
+    uint64_t duration; // milli seconds
+    uint64_t target_time; // micro seconds
+    uint n = 101 - 1; // Num of commands to send on rise and fall
+
+    // Send 0 command for 200 ms
+    throttle_code = THROTTLE_ZERO;
+    telemtry = 0;
+    duration = 200; //ms
+    target_time = timer_hw->timerawl + duration * 1000;
+    // TODO: re implement as a timer interrupt
+    while (timer_hw->timerawl < target_time) send_dshot_frame();
+
+    // Increase throttle from 0 from to ARM_THROTTLE for 100 steps
+    // < 20 ms time for Dshot 150, 20 bit frame length, n = 100
+    n = 100;
+    telemtry = 0;
+    for (uint16_t i = 0; i <= n; ++i){
+        throttle_code = THROTTLE_ZERO + i * (ARM_THROTTLE - THROTTLE_ZERO) / n;
+        send_dshot_frame();
     }
 
-    // a (arm)
-    if (incomingByte == 97)
-    {
+    // Send ARM_THROTTLE command for 300 ms
+    throttle_code = ARM_THROTTLE;
+    telemtry = 0;
+    duration = 300; //ms
+    target_time = timer_hw->timerawl + duration * 1000;
+    // TODO: re implement as a timer interrupt
+    while (timer_hw->timerawl < target_time) send_dshot_frame();
 
-      code_to_dma_buffer(0, 0, pwm_channel);
-
-      dma_channel_configure(
-          dma_chan,
-          &dma_conf,
-          &pwm_hw->slice[pwm_slice_num].cc, // Write to PWM counter compare
-          dma_buffer,
-          dma_buffer_length,
-          true);
+    // Decrease throttle to 48
+    // < 20 ms time for Dshot 150, 20 bit frame length, n = 100
+    n = 100;
+    telemtry = 0;
+    for (uint16_t i = n; i < n; --i){
+        throttle_code = THROTTLE_ZERO + i * (ARM_THROTTLE - THROTTLE_ZERO) / n;
+        send_dshot_frame();
     }
 
-    // s (spin)
-    if (incomingByte == 115)
+    // Send THROTTLE_ZERO for 500 ms
+    throttle_code = THROTTLE_ZERO;
+    telemtry = 0;
+    duration = 500; //ms
+    target_time = timer_hw->timerawl + duration * 1000;
+    // TODO: re implement as a timer interrupt
+    while (timer_hw->timerawl < target_time) send_dshot_frame();
+
+    // while (throttle_code != 0)
+    // {
+    //     send_dshot_frame();
+    //     throttle_code = arm_sequence(arm_idx);
+    //     ++arm_idx;
+    // }
+
+    // Do nothing for some time
+    // delay(2000);
+
+    // do
+    // {
+    //     send_dshot_frame();
+    //     ++arm_idx;
+    //     throttle_code = arm_sequence(arm_idx);
+    // } while (throttle_code != 0);
+
+    // throttle_code = 48;
+
+    Serial.println();
+    Serial.print("Arm idx: ");
+    Serial.println(arm_idx);
+    Serial.print("Writes to temp dma buffer: ");
+    Serial.println(writes_to_temp_dma_buffer);
+    Serial.print("Writes to dma buffer: ");
+    Serial.println(writes_to_dma_buffer);
+}
+
+void loop()
+{
+    if (Serial.available() > 0)
     {
-      // Reset DMA Buffer
-      for (size_t i = 0; i < dma_buffer_length; ++i)
-      {
-        dma_buffer[i] = 0;
-      }
+        incomingByte = Serial.read();
+        Serial.println(incomingByte, DEC);
 
-      // Fill with increasing throttle value
-      size_t dma_buffer_idx = 0;
-      for (size_t i = 48; i < 55; ++i)
-      {
-        DShot::command_to_pwm_buffer(i, 0, dma_buffer + dma_buffer_idx, DSHOT_LOW, DSHOT_HIGH, pwm_channel);
-        dma_buffer_idx += cmd_size;
-      }
+        // l (led)
+        if (incomingByte == 108)
+        {
+            utils::flash_led(LED_BUILTIN);
+        }
 
-      // DShot::command_to_pwm_buffer(750, 0, dma_buffer, DSHOT_LOW, DSHOT_HIGH, pwm_channel);
+        // a (arm)
+        if (incomingByte == 97)
+        {
+            arm_motor();
+            return;
+        }
 
-      dma_channel_configure(
-          dma_chan,
-          &dma_conf,
-          &pwm_hw->slice[pwm_slice_num].cc, // Write to PWM counter compare
-          dma_buffer,
-          dma_buffer_length,
-          true);
+        // b - beep
+        if (incomingByte == 98)
+        {
+            throttle_code = 1;
+            telemtry = 1;
+        }
+
+        // r - ramp
+        if (incomingByte == 115)
+        {
+            // Ramp up to speed = 400
+
+        }
+
+        // spacebar = 0 throttle --> This could be disarm. 
+        // Not sure how to disarm yet. Maybe set throttle to 0 and don't send a cmd for some secs?
+        if (incomingByte == 32)
+        {
+            throttle_code = THROTTLE_ZERO;
+            telemtry = 0;
+        }
+
+        send_dshot_frame();
+        Serial.println("Finished processing byte.");
     }
-
-    // b - beep
-    if (incomingByte == 98)
-    {
-      // Reset DMA Buffer
-      for (size_t i = 0; i < dma_buffer_length; ++i)
-      {
-        dma_buffer[i] = 0;
-      }
-
-      DShot::command_to_pwm_buffer(1, 1, dma_buffer, DSHOT_LOW, DSHOT_HIGH, pwm_channel);
-
-      dma_channel_configure(
-          dma_chan,
-          &dma_conf,
-          &pwm_hw->slice[pwm_slice_num].cc, // Write to PWM counter compare
-          dma_buffer,
-          dma_buffer_length,
-          true);
-    }
-
-    // spacebar - disarm?
-    if (incomingByte == 32)
-    {
-      // Reset DMA Buffer
-      for (size_t i = 0; i < dma_buffer_length; ++i)
-      {
-        dma_buffer[i] = 0;
-      }
-
-      DShot::command_to_pwm_buffer(0, 0, dma_buffer, DSHOT_LOW, DSHOT_HIGH, pwm_channel);
-
-      dma_channel_configure(
-          dma_chan,
-          &dma_conf,
-          &pwm_hw->slice[pwm_slice_num].cc, // Write to PWM counter compare
-          dma_buffer,
-          dma_buffer_length,
-          true);
-    }
-
-    Serial.println(incomingByte, DEC);
-  }
-
-  // digitalWrite(LED_BUILTIN, HIGH);
-  // delay(200);
-
-  // digitalWrite(LED_BUILTIN, LOW);
-  // delay(200);
 }
