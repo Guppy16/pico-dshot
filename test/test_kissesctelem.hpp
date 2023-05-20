6b2a528b78da9de0ee41535601f00b3fc3b62308
Akash <ag2118@cam.ac.uk>
2023-05-20 13:00:06 +0100

    feat: store crc in telem struct

--- test/test_kissesctelem.hpp
+++ test/test_kissesctelem.hpp
@@ -2,26 +2,38 @@
 #include "unity.h"
 #include <stdio.h>
 
-static void test_kissesc_get_crc8(void) {
-  uint8_t buffer[KISS_ESC_TELEM_BUFFER_SIZE] = {0x1c, 0x04, 0xd6, 0x00, 0x00,
-                                                0x00, 0x17, 0x00, 0x00, 0x8f};
-  uint8_t crc = kissesc_get_crc8(buffer, KISS_ESC_TELEM_BUFFER_SIZE);
-  printf("CRC: %.2x\n", crc);
+const uint8_t kissesc_buffer[KISS_ESC_TELEM_BUFFER_SIZE] = {
+    0x1c, 0x04, 0xd6, 0x00, 0x00, 0x00, 0x17, 0x00, 0x00, 0x8f};
 
-  kissesc_telem telem;
-  kissesc_buffer_to_telem(buffer, &telem);
-  kissesc_print(&telem);
+static void test_kissesc_get_crc8(void) {
+
+  uint8_t crc8 = kissesc_get_crc8(kissesc_buffer, KISS_ESC_TELEM_BUFFER_SIZE);
+  TEST_ASSERT_EQUAL(0x00, crc8);
 }
 
 static void test_kissesc_buffer_to_telem(void) {
-  uint8_t buffer[KISS_ESC_TELEM_BUFFER_SIZE] = {0};
   kissesc_telem telem;
-  kissesc_buffer_to_telem(buffer, &telem);
-  kissesc_print(&telem);
+  kissesc_buffer_to_telem(kissesc_buffer, &telem);
+  kissesc_print_telem(&telem);
+
+  kissesc_telem expected_telem{.temperature = 28,
+                               .centi_voltage = 1238,
+                               .centi_current = 0,
+                               .consumption = 23,
+                               .erpm = 0,
+                               .crc = 0};
+  kissesc_print_telem(&expected_telem);
+
+  TEST_ASSERT_EQUAL(expected_telem.temperature, telem.temperature);
+  TEST_ASSERT_EQUAL(expected_telem.centi_voltage, telem.centi_voltage);
+  TEST_ASSERT_EQUAL(expected_telem.centi_current, telem.centi_current);
+  TEST_ASSERT_EQUAL(expected_telem.consumption, telem.consumption);
+  TEST_ASSERT_EQUAL(expected_telem.erpm, telem.erpm);
+  TEST_ASSERT_EQUAL(expected_telem.crc, telem.crc);
 }
 
 static int runUnityTests_kissesctelem(void) {
-  UnityBegin("KISSESCTELEM");
+  UnityBegin("KISSESC_TELEM");
   RUN_TEST(test_kissesc_get_crc8);
   RUN_TEST(test_kissesc_buffer_to_telem);
   return UNITY_END();
